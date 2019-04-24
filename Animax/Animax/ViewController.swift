37b0ff9df9d0e9c9a440d33d233def2398e67866
Angel <angelbuenrostro@outlook.com>
2019-04-24 17:20:48 -0400

    Create Project - IPAD LANDSCAPE ONLY

---
+++ Animax/Animax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animax
+//
+//  Created by Angel Buenrostro on 4/24/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
