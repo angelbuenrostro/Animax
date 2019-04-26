172e5fd6b45fd8be832718ef187d1a3cfd23c505
Angel <angelbuenrostro@outlook.com>
2019-04-26 18:51:14 -0400

    Add export functionality and UI elements for draw functions

---
+++ Animax/Animax/DrawViewController.swift
@@ -0,0 +1,184 @@
+//
+//  ViewController.swift
+//  Animax
+//
+//  Created by Angel Buenrostro on 4/24/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class DrawViewController: UIViewController {
+    
+    
+
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    var isErasing: Bool = false
+
+    @IBOutlet var exportView: UIView!
+    @IBOutlet weak var exportButton: UIButton!
+    @IBOutlet weak var colorWheelButton: UIButton!
+    @IBOutlet weak var colorWheel: ColorWheel!
+    @IBOutlet weak var colorSwatch: UIButton!
+    @IBOutlet weak var optionsButton: UIButton!
+    
+    @IBOutlet weak var drawView: SwiftyDrawView!
+    @IBOutlet weak var eraserButton: UIButton!
+    
+    
+    @IBOutlet weak var brushSizeSlider: UISlider!
+    @IBOutlet weak var brushOpacitySlider: UISlider!
+    
+    @IBOutlet weak var buttonsStackView: UIStackView!
+    @IBOutlet weak var slidersStackView: UIStackView!
+    @IBOutlet weak var colorsStackView: UIStackView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupUI()
+        //drawView.isMultipleTouchEnabled = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        if #available(iOS 9.1, *) {
+            drawView.allowedTouchTypes = [.finger, .pencil]
+        }
+    }
+    
+    func animateIn() {
+        self.view.addSubview(exportView)
+        exportView.center = self.view.center
+        exportView.layer.cornerRadius = 20
+        exportView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        exportView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            self.exportView.alpha = 1
+            self.exportView.transform = CGAffineTransform.identity
+        }
+    }
+
+    @IBAction func optionsButtonTapped(_ sender: Any) {
+        if !self.view.subviews.contains(exportView){
+            animateIn()
+        } else {
+            exportView.removeFromSuperview()
+        }
+    }
+    @IBAction func brushSizeSliderChanged(_ sender: Any) {
+        drawView.brush.width = CGFloat(brushSizeSlider.value)
+    }
+    @IBAction func brushOpacitySliderChanged(_ sender: Any) {
+        drawView.brush.opacity = CGFloat(brushOpacitySlider.value)
+        colorSwatch.alpha = CGFloat(brushOpacitySlider.value)
+    }
+    @IBAction func colorChanged(_ sender: ColorWheel) {
+        drawView.brush.color = sender.color
+        colorSwatch.backgroundColor = sender.color
+    }
+    @IBAction func colorWheelButtonTapped(_ sender: Any) {
+        colorWheel.isHidden = !colorWheel.isHidden
+    }
+    
+    @IBAction func exportButtonTapped(_ sender: Any) {
+        
+        // Get image from drawView and save to User Photo Library
+        
+        guard let image = drawView.getImage() else {
+            
+            //popup view controller saying please draw something first
+            let alertController = UIAlertController(title: "Oops", message: "Draw something first!", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alertController, animated: true, completion: nil)
+            self.exportView.removeFromSuperview()
+            return
+        }
+        
+        
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAsset(from: image)
+        }) { (success, error) in
+            if success {
+                // pop up alert to notify user
+                print("successfully saved")
+                DispatchQueue.main.async {
+                    let alertController = UIAlertController(title: "Saved", message: "Drawing successfully saved to camera roll", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                    self.exportView.removeFromSuperview()
+                }
+            } else if let error = error {
+                // pop up alert to notify user
+            } else {
+                fatalError()
+            }
+        }
+        //
+        
+        print("export tapped")
+    }
+    
+    @IBAction func cancelExportButtonTapped(_ sender: Any) {
+        self.exportView.removeFromSuperview()
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: Any) {
+        drawView.clear()
+    }
+    @IBAction func undoButtonTapped(_ sender: Any) {
+        drawView.undo()
+    }
+    @IBAction func redoButtonTapped(_ sender: Any) {
+        drawView.redo()
+    }
+    @IBAction func eraserButtonTapped(_ sender: Any) {
+        if !isErasing {
+        drawView.brush.blendMode = .clear
+            eraserButton.setTitleColor(.black, for: .normal)
+            isErasing = true
+        } else {
+            eraserButton.setTitleColor(#colorLiteral(red: 0.01202730276, green: 0.3897084594, blue: 0.8242080808, alpha: 1), for: .normal)
+            isErasing = false
+            drawView.brush.blendMode = .normal
+        }
+    }
+    
+    func setupUI(){
+        slidersStackView.layer.cornerRadius = 20
+        slidersStackView.backgroundColor = .black
+        slidersStackView.makeVertical()
+        optionsButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+        var sliderPosition = CGPoint(x: 40 , y: view.bounds.height/2)
+        slidersStackView.center = sliderPosition
+        
+        
+        colorsStackView.center = CGPoint(x:sliderPosition.x, y:sliderPosition.y + colorsStackView.bounds.height + 12)
+        
+        buttonsStackView.center = CGPoint(x: sliderPosition.x, y: buttonsStackView.bounds.height + 12)
+        
+        colorWheel.isHidden = true
+        colorSwatch.clipsToBounds = true
+        colorSwatch.layer.cornerRadius = colorSwatch.bounds.width/2.0
+        
+        exportButton.layer.cornerRadius = 12
+    }
+}
+
+extension DrawViewController: SwiftyDrawViewDelegate {
+    
+    func swiftyDraw(shouldBeginDrawingIn drawingView: SwiftyDrawView, using touch: UITouch) -> Bool { return true }
+    func swiftyDraw(didBeginDrawingIn drawingView: SwiftyDrawView, using touch: UITouch) {  }
+    func swiftyDraw(isDrawingIn drawingView: SwiftyDrawView, using touch: UITouch) {  }
+    func swiftyDraw(didFinishDrawingIn drawingView: SwiftyDrawView, using touch: UITouch) {  }
+    func swiftyDraw(didCancelDrawingIn drawingView: SwiftyDrawView, using touch: UITouch) {  }
+}
+
+
+
---
+++ Animax/SwiftyDraw/Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  Extensions.swift
+//  SwiftyDrawExample
+//
+//  Created by Linus Geffarth on 22.02.19.
+//  Copyright © 2019 Walzy. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        set { layer.cornerRadius = newValue }
+        get { return layer.cornerRadius }
+    }
+}
+
+extension UIView
+{
+    func makeVertical()
+    {
+        transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+    }
+}
+
+
+
